2952fcb9564bff74b381b5ec12d912a938e66a12
dimadim4ik <dimadim4ik@gmail.com>
2021-11-24 23:41:17 +0200

    orders & user relation

---
+++ app/models/orders_description.rb
@@ -0,0 +1,4 @@
+class OrdersDescription < ApplicationRecord
+
+    belongs_to :order
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_221320) do
+ActiveRecord::Schema.define(version: 2021_11_24_211016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,21 @@ ActiveRecord::Schema.define(version: 2021_11_17_221320) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_items_on_name"
     t.index ["price"], name: "index_items_on_price"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.float "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders_descriptions", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "item_id"
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
