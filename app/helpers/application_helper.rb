d254dfb48dae658fcaf369dc871673a8bfcd9a16
dimadim4ik <dimadim4ik@gmail.com>
2021-11-25 21:02:07 +0200

    create devise views & helpers check & partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+    def urls_to_links(s)
+        s.gsub! /\s(http:\/\/.*?)\s/,
+            '<p><a href="\1">\1</a></p>'
+            
+        s.html_safe
+    end
 end
